e0bc55789ea4e9c331a3a06082ec124c10c83825
HutchEllis <96447758+HutchEllis@users.noreply.github.com>
2021-12-22 09:19:43 -0800

    Update 03_01 Exercise.py

--- Exercises/03_01 Exercise.py
+++ Exercises/03_01 Exercise.py
@@ -3,12 +3,17 @@ rate= float(input("Enter Rate: "))
 pay = hours*rate
 print("Regular Pay: ",pay)
 ot= float(hours-40)
-otrate= float(rate*0.5)
-otpay= float(ot*otrate)
+otr= float(rate*0.5)
+otrate= float(otr+rate)
+otpay= float(ot*otr)
 if (ot>0.01):  
     print("Overtime Hours: ", ot)
     print("Overtime Rate: ", otrate)
     print("Overtime Pay: ", otpay)
     print("Total Pay: ",otpay+pay)
+elif (ot<0.01):
+    print("Overtime Hours: 0")
+    print("Overtime Pay: 0")
+    print("Total Pay: ", pay)
 
 
